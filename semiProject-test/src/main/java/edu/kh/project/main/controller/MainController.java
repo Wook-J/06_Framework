2691b0c08635b116134874c584040a92454d0a3a
Wook-J <dnrwo0827@naver.com>
2024-11-13 19:52:06 +0900

    241113 오후 8시

--- semiProject-test/src/main/java/edu/kh/project/main/controller/MainController.java
+++ semiProject-test/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,7 +1,9 @@
 package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -10,4 +12,11 @@ public class MainController {
 	public String mainPage() {
 		return "common/main";
 	}
+	
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message", "로그인 후 이용해 주세요");
+		return "redirect:/";
+	}
+	
 }
